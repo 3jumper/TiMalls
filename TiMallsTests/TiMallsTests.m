2a321bb9665c52e719adff7bf5a2662e372914d1
hayato <triple.jumper1829@gmail.com>
2014-06-11 19:26:24 +0800

    init

---
+++ TiMallsTests/TiMallsTests.m
@@ -0,0 +1,34 @@
+//
+//  TiMallsTests.m
+//  TiMallsTests
+//
+//  Created by hayato on 3/23/14.
+//  Copyright (c) 2014 hayato miyoshi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TiMallsTests : XCTestCase
+
+@end
+
+@implementation TiMallsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
